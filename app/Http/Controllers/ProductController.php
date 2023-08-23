93b3b7649600b4eb5a54aa950564793e86185177
Anna Pavlenko <annapavlenko637@gmail.com>
2023-08-23 18:17:09 +0300

    Update data using API | Delete data using API in Laravel & Postman

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -41,4 +41,41 @@ class ProductController extends Controller
         $product->save();
         return response()->json(['message'=>'Product Added Successfully'], 200);
     }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+        'name'=>'required|max:191',
+        'description'=>'required|max:191',
+        'price'=>'required|max:191',
+        'qty'=>'required|max:191',
+        ]);
+
+        $product =  Product::find($id);
+        if($product)
+        {
+        $product->name = $request->name;
+        $product->description= $request->description;
+        $product->price= $request->price;
+        $product->qty= $request->qty;
+        $product->update();
+        return response()->json(['message'=>'Product Updated Successfully'], 200);
+        }
+        else{
+            return response()->json(['message'=>'No product found'],404);  
+        }
+    }
+
+    public function delete($id)
+    {
+        $product =  Product::find($id);
+        if($product)
+        {
+            $product->delete();
+            return response()->json(['message'=>'Product Delete Successfully'], 200);
+        }
+        else{
+            return response()->json(['message'=>'No product found'],404);  
+        }
+    }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('products',[ProductController::class, 'index']);
 Route::get('product/{id}/show',[ProductController::class, 'show']);
+Route::post('product/{id}/update',[ProductController::class, 'update']);
+Route::delete('product/{id}/delete',[ProductController::class, 'delete']);
 Route::post('product/add',[ProductController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
