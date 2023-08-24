872b7848d8076b74c16c5e42405d3ece14684397
Anna Pavlenko <annapavlenko637@gmail.com>
2023-08-24 14:42:12 +0300

    Authentication with Laravel Sanctum | Login, Register & logout with Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\ProductController;
-use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('login',[AuthController::class,'login']);
+Route::post('register',[AuthController::class,'register']);
+
+Route::middleware(['auth:sanctum'])->group(function(){
+Route::post('logout',[AuthController::class,'logout']);
 Route::get('products',[ProductController::class, 'index']);
 Route::get('product/{id}/show',[ProductController::class, 'show']);
 Route::post('product/{id}/update',[ProductController::class, 'update']);
@@ -12,4 +17,5 @@ Route::delete('product/{id}/delete',[ProductController::class, 'delete']);
 Route::post('product/add',[ProductController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
 });
